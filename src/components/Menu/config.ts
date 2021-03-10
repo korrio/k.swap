2b0ea01b9b20923a528a8913e5e16875ec1b4bee
korrio <korr@aq1.co>
2021-03-10 20:20:25 +0700

    k.swap init

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,85 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'http://localhost:3001/#/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: 'http://localhost:3001/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Nests',
+    icon: 'PoolIcon',
+    href: '/nests',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/goosedefi/',
+      },
+      {
+        label: 'Docs',
+        href: 'https://goosedefi.gitbook.io/goose-finance/',
+      },
+      {
+        label: 'Blog',
+        href: 'https://goosefinance.medium.com/',
+      },
+    ],
+  },
+  {
+    label: 'Partnerships/IFO',
+    icon: 'GooseIcon',
+    href:
+      'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
+  },
+  {
+    label: 'Audit by Hacken',
+    icon: 'AuditIcon',
+    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
+  },
+  {
+    label: 'Audit by CertiK',
+    icon: 'AuditIcon',
+    href: 'https://certik.org/projects/goose-finance',
+  },
+]
+
+export default config
